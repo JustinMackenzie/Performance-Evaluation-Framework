63680270da67123f22e090bbc153dee17a63ddb4
JustinMackenzie <jmackenzie1908@gmail.com>
2015-01-12 00:33:11 -0500

    Added abstract state chart and state chart event to core project to follow DIP.

---
+++ Core/IStateChart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScenarioSim.Core
+{
+    public interface IStateChart
+    {
+        void Dispatch(IStateChartEvent e);
+
+        bool IsStateActive(string name);
+
+        IList<string> ActiveStates();
+
+        void Start();
+    }
+}
