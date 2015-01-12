592e885e9aac151988b5fc93a1a9334e68042a99
JustinMackenzie <jmackenzie1908@gmail.com>
2015-01-12 00:34:11 -0500

    Added facade class to implement the IStateChart interface and implemented IStateChartEvent with StateChartEvent.

--- StateChart/StateChartEvent.cs
+++ StateChart/StateChartEvent.cs
@@ -1,19 +1,28 @@
 using System;
+using ScenarioSim.Core;
 
 namespace ScenarioSim.StateChart
 {
-    public class StateChartEvent
+    public class StateChartEvent : IStateChartEvent
     {
         public int Id { get; set; }
+        public string Name { get; set; }
 
         public StateChartEvent()
         {
-
+            Name = string.Empty;
         }
 
         public StateChartEvent(int id)
         {
             Id = id;
+            Name = string.Empty;
+        }
+
+        public StateChartEvent(int id, string name)
+        {
+            Id = id;
+            Name = name;
         }
 
         public Boolean Equals(StateChartEvent stateChartEvent, StateDataContainer dataContainer)
