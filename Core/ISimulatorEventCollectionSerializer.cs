a95ca4dd442f248f4ac9dc884cf90e83fd3d2046
JustinMackenzie <jmackenzie1908@gmail.com>
2015-01-26 20:48:43 -0500

    Implemented Xml serialization of simulator event collection.

---
+++ Core/ISimulatorEventCollectionSerializer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ScenarioSim.Core
+{
+    interface ISimulatorEventCollectionSerializer
+    {
+        void Serialize(string filename, SimulatorEventCollection collection);
+        SimulatorEventCollection Deserialize(string filename);
+    }
+}
---
+++ Core/XmlSimulatorEventCollectionSerializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ScenarioSim.Core
+{
+    class XmlSimulatorEventCollectionSerializer : ISimulatorEventCollectionSerializer
+    {
+        public void Serialize(string filename, SimulatorEventCollection collection)
+        {
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                XmlSerializer serializer = new XmlSerializer(collection.GetType());
+                serializer.Serialize(writer, collection);
+            }
+        }
+
+
+        public SimulatorEventCollection Deserialize(string filename)
+        {
+            using (XmlTextReader reader = new XmlTextReader(filename))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(SimulatorEventCollection));
+                return serializer.Deserialize(reader) as SimulatorEventCollection;
+            }
+        }
+    }
+}
