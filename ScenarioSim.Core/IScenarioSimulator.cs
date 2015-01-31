0fbceaa90ffa41d74068e4920834f10a413a015f
JustinMackenzie <jmackenzie1908@gmail.com>
2015-01-31 00:52:21 -0500

    Changed directory name for core project.

--- ScenarioSim.Core/IScenarioSimulator.cs
+++ ScenarioSim.Core/IScenarioSimulator.cs
@@ -2,7 +2,7 @@
 namespace ScenarioSim.Core
 {
     public interface IScenarioSimulator
-    {
+    {  
         void AddTrackedParameter(int eventId, string parameterName);
         void Start();
         void SubmitSimulatorEvent(SimulatorEvent e);
