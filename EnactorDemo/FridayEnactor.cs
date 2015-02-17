bf14d76e9b7fd88f3e605a327a36b1ece45edd61
JustinMackenzie <jmackenzie1908@gmail.com>
2015-02-16 20:54:15 -0500

    Added enactor demo project to display the design pattern.

---
+++ EnactorDemo/FridayEnactor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnactorDemo
+{
+    class FridayEnactor : INewDayEnactor
+    {
+        public void Enact(NewDayEvent o)
+        {
+            Console.WriteLine("TGIF!");
+        }
+
+        public DayOfWeek Day
+        {
+            get { return DayOfWeek.Friday; }
+        }
+    }
+}
---
+++ EnactorDemo/MondayEnactor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnactorDemo
+{
+    class MondayEnactor : INewDayEnactor
+    {
+        public void Enact(NewDayEvent o)
+        {
+            Console.WriteLine("Oh no it's Monday!");
+        }
+
+        public DayOfWeek Day
+        {
+            get { return DayOfWeek.Monday; }
+        }
+    }
+}
