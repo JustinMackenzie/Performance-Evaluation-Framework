84b336646a0893b7d1eb06930643e08808619f7f
JustinMackenzie <jmackenzie1908@gmail.com>
2015-02-16 20:55:49 -0500

    Added some forgotten files for the enactor demo.

---
+++ EnactorDemo/CalendarClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnactorDemo
+{
+    class CalendarClient
+    {
+        public CalendarClient(NewDayInvoker invoker)
+        {
+            invoker.RegisterEnactor(new MondayEnactor());
+            invoker.RegisterEnactor(new FridayEnactor());
+        }
+
+        public static void Main()
+        {
+            NewDayInvoker invoker = new NewDayInvoker();
+            CalendarClient client = new CalendarClient(invoker);
+
+            invoker.NewDay(new NewDayEvent() { Day = DayOfWeek.Monday });
+            invoker.NewDay(new NewDayEvent() { Day = DayOfWeek.Friday });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EnactorDemo/Invoker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace EnactorDemo
+{
+    class NewDayInvoker
+    {
+        Dictionary<DayOfWeek, INewDayEnactor> enactors;
+
+        public NewDayInvoker()
+        {
+            enactors = new Dictionary<DayOfWeek, INewDayEnactor>();
+        }
+
+        public void RegisterEnactor(INewDayEnactor enactor)
+        {
+            enactors.Add(enactor.Day, enactor);
+        }
+
+        public void NewDay(NewDayEvent e)
+        {
+            if(enactors.ContainsKey(e.Day))
+                enactors[e.Day].Enact(e);
+        }
+    }
+}
