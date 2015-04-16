fb480e1f93dd72a7ab32f6ce57b0114d4424978b
JustinMackenzie <jmackenzie1908@gmail.com>
2015-04-16 08:08:15 -0400

    Updated documentation for core interfaces package.

--- ScenarioSim.Core.Interfaces/IScenarioEventRepository.cs
+++ ScenarioSim.Core.Interfaces/IScenarioEventRepository.cs
@@ -2,9 +2,20 @@
 
 namespace ScenarioSim.Core.Interfaces
 {
+    /// <summary>
+    /// An interface that provides the means to store and retrieve scenario event entities.
+    /// </summary>
     public interface IScenarioEventRepository
     {
+        /// <summary>
+        /// Returns all of the scenario events in the repository.
+        /// </summary>
         ScenarioEventCollection Events { get; }
+
+        /// <summary>
+        /// Stores the given scenario event in the repository.
+        /// </summary>
+        /// <param name="e"></param>
         void Save(ScenarioEvent e);
     }
 }
