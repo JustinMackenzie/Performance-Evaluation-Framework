b60ae15d7b6fce3f6577d9a68fcbb1de41e8a478
JustinMackenzie <jmackenzie1908@gmail.com>
2015-04-16 16:16:48 -0400

    Completed the documentation for the playback service package.

--- ScenarioSim.Services.Playback/IEventEnactor.cs
+++ ScenarioSim.Services.Playback/IEventEnactor.cs
@@ -2,9 +2,20 @@
 
 namespace ScenarioSim.Services.Playback
 {
+    /// <summary>
+    /// The event enactor is responsible for playing out an event.
+    /// </summary>
     public interface IEventEnactor
     {
+        /// <summary>
+        /// The identifier of the event that this enactor represents.
+        /// </summary>
         int EventId { get; set; }
+
+        /// <summary>
+        /// Enacts the event.
+        /// </summary>
+        /// <param name="e">The event to enact.</param>
         void Enact(ScenarioEvent e);
     }
 }
