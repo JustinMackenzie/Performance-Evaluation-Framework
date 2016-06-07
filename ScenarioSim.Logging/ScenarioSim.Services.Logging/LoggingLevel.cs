b4d9365e913473463922c62ad0d9243340b6545a
Justin Mackenzie <jmackenzie1908@gmail.com>
2016-06-07 00:20:20 -0400

    Added logging level code file.

---
+++ ScenarioSim.Logging/ScenarioSim.Services.Logging/LoggingLevel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScenarioSim.Services.Logging
+{
+    public enum LoggingLevel
+    {
+        Trace,
+        Debug,
+        Info,
+        Warning,
+        Error
+    }
+}
