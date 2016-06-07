68efd3861853a4baff3f96e7f3bce81b47ba6329
Justin Mackenzie <jmackenzie1908@gmail.com>
2016-06-07 00:59:50 -0400

    Added user repository interface.

---
+++ ScenarioSim.Core/ScenarioSim.Core.Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ScenarioSim.Core.Entities;
+
+namespace ScenarioSim.Core.Interfaces
+{
+    public interface IUserRepository
+    {
+        IEnumerable<User> GetAll();
+        User Get(int id);
+        void Save(User user);
+        void Remove(int id);
+    }
+}
