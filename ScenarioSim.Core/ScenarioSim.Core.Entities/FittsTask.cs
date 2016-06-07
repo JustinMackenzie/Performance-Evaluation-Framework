644364dd39854595a83cb517a43ebb54e467b09d
Justin Mackenzie <jmackenzie1908@gmail.com>
2016-06-07 01:15:23 -0400

    Implemented Fitts' Task class.

---
+++ ScenarioSim.Core/ScenarioSim.Core.Entities/FittsTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScenarioSim.Core.Entities
+{
+    /// <summary>
+    /// Represents a task in the Fitts' Law paradigm
+    /// </summary>
+    /// <seealso cref="ScenarioSim.Core.Entities.Task" />
+    public class FittsTask : Task
+    {
+        /// <summary>
+        /// Gets or sets the D parameter.
+        /// </summary>
+        /// <value>
+        /// The D parameter in the Fitts' Law equation.
+        /// </value>
+        public float D { get; set; }
+
+        /// <summary>
+        /// Gets or sets the W parameter.
+        /// </summary>
+        /// <value>
+        /// The W parameter in the Fitts' Law equation.
+        /// </value>
+        public float W { get; set; }
+
+        /// <summary>
+        /// Gets the index of difficulty.
+        /// </summary>
+        /// <value>
+        /// The index of difficulty.
+        /// </value>
+        public float IndexOfDifficulty => (float)Math.Log(2.0 * D / W, 2);
+    }
+}
