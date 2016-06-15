9a6a483579d4d26e133575cf6c66dcaa20109f02
Justin Mackenzie <jmackenzie1908@gmail.com>
2016-06-14 23:38:01 -0400

    Implemented JSON.NET serializer.

---
+++ ScenarioSim.Serialization/ScenarioSim.Infrastructure.JsonNetSerializer/JsonNetFileSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using ScenarioSim.Services.Serialization;
+
+namespace ScenarioSim.Infrastructure.JsonNetSerializer
+{
+    public class JsonNetFileSerializer : IFileSerializer
+    {
+        public T Deserialize<T>(string filename)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+
+            using (StreamReader reader = new StreamReader(filename))
+            using (JsonReader r = new JsonTextReader(reader))
+                return (T)serializer.Deserialize(r);
+        }
+
+        public void Serialize<T>(string filename, T value)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+
+            using (StreamWriter streamWriter = new StreamWriter(filename))
+                serializer.Serialize(streamWriter, value);
+        }
+    }
+}
---
+++ ScenarioSim.Serialization/ScenarioSim.Infrastructure.JsonNetSerializer/JsonNetSerializer.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using ScenarioSim.Services.Serialization;
+
+namespace ScenarioSim.Infrastructure.JsonNetSerializer
+{
+    public class JsonNetSerializer : ISerializer
+    {
+        public string Serialize<T>(T value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+
+        public T Deserialize<T>(string value)
+        {
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
