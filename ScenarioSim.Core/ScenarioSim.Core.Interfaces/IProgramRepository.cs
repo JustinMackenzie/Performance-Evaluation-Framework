340d1e89aef0bf13411e07e80aef450f6f8b1eb3
Justin Mackenzie <jmackenzie1908@gmail.com>
2016-06-17 12:27:59 -0400

    Added program repository interface.

---
+++ ScenarioSim.Core/ScenarioSim.Core.Interfaces/IProgramRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using ScenarioSim.Core.Entities;
+
+namespace ScenarioSim.Core.Interfaces
+{
+    /// <summary>
+    /// Used to retrieve and store programs.
+    /// </summary>
+    public interface IProgramRepository
+    {
+        /// <summary>
+        /// Gets all programs.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Program> GetAll();
+
+        /// <summary>
+        /// Gets the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        Program Get(Guid id);
+
+        /// <summary>
+        /// Saves the specified program.
+        /// </summary>
+        /// <param name="program">The program.</param>
+        void Save(Program program);
+
+    }
+}
