6a55eff2aae3497b1f490e39a685e795ff65f2ca
Justin Mackenzie <jmackenzie1908@gmail.com>
2016-06-17 07:41:17 -0400

    Added schema repository interface.

---
+++ ScenarioSim.Core/ScenarioSim.Core.Interfaces/ISchemaRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using ScenarioSim.Core.Entities;
+
+namespace ScenarioSim.Core.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ISchemaRepository
+    {
+        /// <summary>
+        /// Gets all.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Schema> GetAll();
+
+        /// <summary>
+        /// Gets the specified unique identifier.
+        /// </summary>
+        /// <param name="guid">The unique identifier.</param>
+        /// <returns></returns>
+        Schema Get(Guid guid);
+
+        /// <summary>
+        /// Saves the specified schema.
+        /// </summary>
+        /// <param name="schema">The schema.</param>
+        void Save(Schema schema);
+
+        /// <summary>
+        /// Removes the specified schema.
+        /// </summary>
+        /// <param name="schema">The schema.</param>
+        void Remove(Schema schema);
+    }
+}
