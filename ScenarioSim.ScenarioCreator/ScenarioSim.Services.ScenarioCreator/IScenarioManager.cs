eb40761e5183f8f35b80bffd05b2651004ff7b26
Justin Mackenzie <jmackenzie1908@gmail.com>
2016-07-03 23:35:37 -0400

    Implemented scenario creator service interfaces.
    Implemented the asset, scenario and program manager interfaces.

---
+++ ScenarioSim.ScenarioCreator/ScenarioSim.Services.ScenarioCreator/IScenarioManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using ScenarioSim.Core.Entities;
+
+namespace ScenarioSim.Services.ScenarioCreator
+{
+    /// <summary>
+    /// A service used to manage scenario entities.
+    /// </summary>
+    public interface IScenarioManager
+    {
+        /// <summary>
+        /// Gets all the scenarios.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Scenario> GetAllScenarios();
+
+        /// <summary>
+        /// Gets the scenario.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        Scenario GetScenario(Guid id);
+
+        /// <summary>
+        /// Creates the scenario.
+        /// </summary>
+        /// <param name="scenario">The scenario.</param>
+        void CreateScenario(Scenario scenario);
+
+        /// <summary>
+        /// Updates the scenario.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="scenario">The scenario.</param>
+        void UpdateScenario(Guid id, Scenario scenario);
+
+        /// <summary>
+        /// Deletes the scenario.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        void DeleteScenario(Guid id);
+    }
+}
