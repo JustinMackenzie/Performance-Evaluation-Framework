65b26dce005ab481c95cd173c0e472dc843e2738
Justin Mackenzie <jmackenzie1908@gmail.com>
2016-07-22 15:23:16 -0400

    Added performance processing

---
+++ ScenarioSim.PerformanceProcessing/ScenarioSim.Services.PerformanceProcessing/IPerformanceProcessor.cs
@@ -0,0 +1,16 @@
+﻿using ScenarioSim.Core.Entities;
+
+namespace ScenarioSim.Services.PerformanceProcessing
+{
+    /// <summary>
+    /// An abstraction to perform a process on a performance.
+    /// </summary>
+    public interface IPerformanceProcessor 
+    {
+        /// <summary>
+        /// Processes the specified performance.
+        /// </summary>
+        /// <param name="performance">The performance.</param>
+        void Process(ScenarioPerformance performance);
+    }
+}
