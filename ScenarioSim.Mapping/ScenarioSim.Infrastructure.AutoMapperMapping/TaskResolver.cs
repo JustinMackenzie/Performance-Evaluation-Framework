1ce555b9cf866130bfa4922479232bf6e8e0340d
Justin Mackenzie <jmackenzie1908@gmail.com>
2016-10-13 23:41:53 -0400

    Refactored AutoMapper infrastructure project.

---
+++ ScenarioSim.Mapping/ScenarioSim.Infrastructure.AutoMapperMapping/TaskResolver.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using ScenarioSim.Core.Entities;
+using Schema = ScenarioSim.Core.DataTransfer.Schema;
+using Task = ScenarioSim.Core.DataTransfer.Task;
+
+namespace ScenarioSim.Infrastructure.AutoMapperMapping
+{
+    /// <summary>
+    /// Resolves a flat list of tasks to a hierarchical representation.
+    /// </summary>
+    public class TaskResolver : IValueResolver<Schema, Core.Entities.Schema, Core.Entities.Task>
+    {
+        /// <summary>
+        /// Implementors use source object to provide a destination object.
+        /// </summary>
+        /// <param name="source">Source object</param>
+        /// <param name="destination">Destination object, if exists</param>
+        /// <param name="destMember">Destination member</param>
+        /// <param name="context">The context of the mapping</param>
+        /// <returns>
+        /// Result, typically build from the source resolution result
+        /// </returns>
+        public Core.Entities.Task Resolve(Schema source, Core.Entities.Schema destination, Core.Entities.Task destMember, ResolutionContext context)
+        {
+            Task rootTask = source.Tasks.SingleOrDefault(t => t.ParentTaskId == null);
+            return MakeTask(rootTask, source.Tasks, context);
+        }
+
+        /// <summary>
+        /// Makes the task.
+        /// </summary>
+        /// <param name="taskTransferObject">The task transfer object.</param>
+        /// <param name="sourceTasks">The source tasks.</param>
+        /// <param name="context">The context.</param>
+        /// <returns></returns>
+        /// <exception cref="System.InvalidOperationException"></exception>
+        private Core.Entities.Task MakeTask(Task taskTransferObject, List<Task> sourceTasks, ResolutionContext context)
+        {
+            Core.Entities.Task task = null;
+
+            switch (taskTransferObject.TaskType)
+            {
+                case "Task":
+                    task = context.Mapper.Map<Task, Core.Entities.Task>(taskTransferObject);
+                    break;
+                case "CompositeTask":
+                    task = context.Mapper.Map<Task, CompositeTask>(taskTransferObject);
+
+                    CompositeTask compositeTask = (CompositeTask) task;
+                    compositeTask.Tasks = new List<Core.Entities.Task>();
+
+                    foreach (Task model in sourceTasks.Where(t => t.ParentTaskId == taskTransferObject.Id))
+                    {
+                        compositeTask.Tasks.Add(MakeTask(model, sourceTasks, context));
+                    }
+                    break;
+                case "ParallelTask":
+                    task = context.Mapper.Map<Task, ParallelTask>(taskTransferObject);
+
+                    ParallelTask parallelTask = (ParallelTask)task;
+                    parallelTask.Tasks = new List<Core.Entities.Task>();
+
+                    foreach (Task model in sourceTasks.Where(t => t.ParentTaskId == taskTransferObject.Id))
+                    {
+                        parallelTask.Tasks.Add(MakeTask(model, sourceTasks, context));
+                    }
+                    break;
+                default:
+                    throw new InvalidOperationException($"Unknown task type '{taskTransferObject.TaskType}' received.");
+            }
+
+            return task;
+        }
+    }
+}
---
+++ ScenarioSim.Mapping/ScenarioSim.Infrastructure.AutoMapperMapping/TaskValuesResolver.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using AutoMapper;
+using ScenarioSim.Core.DataTransfer;
+using ScenarioSim.Core.Entities;
+
+namespace ScenarioSim.Infrastructure.AutoMapperMapping
+{
+    /// <summary>
+    /// Resolves task values polymorphic relationship based off of a dictionary of values representation.
+    /// </summary>
+    public class TaskValuesResolver : IValueResolver<TaskDefinition, ScenarioTaskDefinition, TaskValues>
+    {
+        /// <summary>
+        /// Implementors use source object to provide a destination object.
+        /// </summary>
+        /// <param name="source">Source object</param>
+        /// <param name="destination">Destination object, if exists</param>
+        /// <param name="destMember">Destination member</param>
+        /// <param name="context">The context of the mapping</param>
+        /// <returns>
+        /// Result, typically build from the source resolution result
+        /// </returns>
+        public TaskValues Resolve(TaskDefinition source, ScenarioTaskDefinition destination, TaskValues destMember,
+            ResolutionContext context)
+        {
+            TaskValues taskValues = null;
+
+            switch (source.TaskValuesType)
+            {
+                case "FittsTaskValues":
+                    taskValues = new FittsTaskValues(float.Parse(source.TaskValues["D"]),
+                        float.Parse(source.TaskValues["W"]));
+                    break;
+                case "ReactionTaskValues":
+                    taskValues = new ReactionTaskValues(float.Parse(source.TaskValues["Delay"]));
+                    break;
+                case "RandomReactionTaskValues":
+                    taskValues = new RandomReactionTaskValues
+                    {
+                        MeanDelay = float.Parse(source.TaskValues["MeanDelay"]),
+                        VarianceDelay = float.Parse(source.TaskValues["VarianceDelay"])
+                    };
+                    break;
+                case "DynamicFittsTaskValues":
+                    taskValues = new DynamicFittsTaskValues
+                    {
+                        EventName = source.TaskValues["EventName"],
+                        ParameterName = source.TaskValues["ParameterName"],
+                        Target = Vector3f.Parse(source.TaskValues["Target"])
+                    };
+                    break;
+                case "SteeringTaskValues":
+                    taskValues = new SteeringTaskValues(float.Parse(source.TaskValues["A"]),
+                        float.Parse(source.TaskValues["W"]));
+                    break;
+                default:
+                    throw new InvalidOperationException($"The task values of type '{source.TaskValuesType} is unsupported.'");
+            }
+
+            return taskValues;
+        }
+    }
+}
