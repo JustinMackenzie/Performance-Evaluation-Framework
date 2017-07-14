7aa3fdeafdbc7f847063f0ab4b1717ff2fdb9cb2
Justin Mackenzie <jmackenzie1908@gmail.com>
2017-07-14 01:51:59 -0400

    Added comments to ITrialRepository. (#7)

--- src/Services/TrialManagement/TrialManagement.Domain/ITrialRepository.cs
+++ src/Services/TrialManagement/TrialManagement.Domain/ITrialRepository.cs
@@ -3,10 +3,23 @@ using TrialManagement.Domain.SeedWork;
 
 namespace TrialManagement.Domain
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="TrialManagement.Domain.SeedWork.IRepository{TrialManagement.Domain.Trial}" />
     public interface ITrialRepository : IRepository<Trial>
     {
+        /// <summary>
+        /// Gets the trial with the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
         Trial Get(Guid id);
 
+        /// <summary>
+        /// Adds the specified trial.
+        /// </summary>
+        /// <param name="trial">The trial.</param>
         void Add(Trial trial);
     }
 }
