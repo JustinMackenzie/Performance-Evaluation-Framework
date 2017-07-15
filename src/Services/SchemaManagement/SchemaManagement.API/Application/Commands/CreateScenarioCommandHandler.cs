43e0fc52113401790885c12a5aa280b934a818d3
Justin Mackenzie <jmackenzie1908@gmail.com>
2017-07-15 15:44:31 -0400

    Added doc comments to create scenario command handler.

--- src/Services/SchemaManagement/SchemaManagement.API/Application/Commands/CreateScenarioCommandHandler.cs
+++ src/Services/SchemaManagement/SchemaManagement.API/Application/Commands/CreateScenarioCommandHandler.cs
@@ -6,18 +6,38 @@ using Task = System.Threading.Tasks.Task;
 
 namespace SchemaManagement.API.Application.Commands
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="MediatR.IAsyncRequestHandler{SchemaManagement.API.Application.Commands.CreateScenarioCommand}" />
     public class CreateScenarioCommandHandler : IAsyncRequestHandler<CreateScenarioCommand>
     {
+        /// <summary>
+        /// The repository
+        /// </summary>
         private readonly ISchemaRepository _repository;
 
+        /// <summary>
+        /// The event bus
+        /// </summary>
         private readonly IEventBus _eventBus;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateScenarioCommandHandler"/> class.
+        /// </summary>
+        /// <param name="repository">The repository.</param>
+        /// <param name="eventBus">The event bus.</param>
         public CreateScenarioCommandHandler(ISchemaRepository repository, IEventBus eventBus)
         {
             this._repository = repository;
             _eventBus = eventBus;
         }
 
+        /// <summary>
+        /// Handles the specified message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns></returns>
         public Task Handle(CreateScenarioCommand message)
         {
             Schema schema = this._repository.Get(message.SchemaId);
