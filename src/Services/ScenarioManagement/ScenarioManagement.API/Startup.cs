d8b8c640d58d5cdda32fa250344a91623c1134d0
Justin Mackenzie <jmackenzie1908@gmail.com>
2017-07-15 22:00:29 -0400

    Implement subscription of scenario created event in scenario management service.

--- src/Services/ScenarioManagement/ScenarioManagement.API/Startup.cs
+++ src/Services/ScenarioManagement/ScenarioManagement.API/Startup.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BuildingBlocks.EventBus.Abstractions;
+using EventBus.RawRabbit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RawRabbit.Serialization;
+using RawRabbit.vNext;
 using ScenarioManagement.API.Application.Queries;
+using ScenarioManagement.API.IntegrationEvents.EventHandling;
+using ScenarioManagement.API.IntegrationEvents.Events;
 using ScenarioManagement.Domain;
 using ScenarioManagement.Infrastructure;
 
@@ -32,12 +39,17 @@ namespace ScenarioManagement.API
         {
             // Add framework services.
             services.AddMvc();
+            services.AddRawRabbit(cfg => cfg.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("rawrabbit.json"),
+                ioc => ioc.AddSingleton<IMessageSerializer, TypelessJsonSerializer>());
 
             services.AddTransient<IScenarioRepository, ScenarioRepository>(provder =>
                 new ScenarioRepository(Configuration.GetConnectionString("ScenarioDatabase"), "scenario-context"));
             services.AddTransient<ITrialSetRepository, TrialSetRepository>(provder =>
                 new TrialSetRepository(Configuration.GetConnectionString("ScenarioDatabase"), "scenario-context"));
             services.AddTransient<ITrialSetQueries, TrialSetQueries>();
+            services.AddSingleton<IRawRabbitSubscriptionRepository, InMemoryRawRabbitSubscriptionRepository>();
+            services.AddSingleton<IEventBus, RawRabbitEventBus>();
+            services.AddTransient<ScenarioCreatedIntegrationEventHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +59,15 @@ namespace ScenarioManagement.API
             loggerFactory.AddDebug();
 
             app.UseMvc();
+            this.ConfigureEventBus(app);
+        }
+
+        protected void ConfigureEventBus(IApplicationBuilder app)
+        {
+            IEventBus eventBus = app.ApplicationServices.GetRequiredService<IEventBus>();
+
+            eventBus.Subscribe<ScenarioCreatedIntegrationEvent, ScenarioCreatedIntegrationEventHandler>
+                (() => app.ApplicationServices.GetRequiredService<ScenarioCreatedIntegrationEventHandler>());
         }
     }
 }
