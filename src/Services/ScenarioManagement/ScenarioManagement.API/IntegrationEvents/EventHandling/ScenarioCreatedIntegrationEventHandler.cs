b8cd09f0b64cbad332ccaf87a27b84ffcde6ad43
Justin Mackenzie <jmackenzie1908@gmail.com>
2017-07-15 22:00:29 -0400

    Implement subscription of scenario created event in scenario management service. (#14)

---
+++ src/Services/ScenarioManagement/ScenarioManagement.API/IntegrationEvents/EventHandling/ScenarioCreatedIntegrationEventHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BuildingBlocks.EventBus.Abstractions;
+using ScenarioManagement.API.IntegrationEvents.Events;
+using ScenarioManagement.Domain;
+
+namespace ScenarioManagement.API.IntegrationEvents.EventHandling
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="BuildingBlocks.EventBus.Abstractions.IIntegrationEventHandler{ScenarioManagement.API.IntegrationEvents.Events.ScenarioCreatedIntegrationEvent}" />
+    public class ScenarioCreatedIntegrationEventHandler : IIntegrationEventHandler<ScenarioCreatedIntegrationEvent>
+    {
+        /// <summary>
+        /// The scenario repository
+        /// </summary>
+        private readonly IScenarioRepository _scenarioRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ScenarioCreatedIntegrationEventHandler"/> class.
+        /// </summary>
+        /// <param name="scenarioRepository">The scenario repository.</param>
+        public ScenarioCreatedIntegrationEventHandler(IScenarioRepository scenarioRepository)
+        {
+            this._scenarioRepository = scenarioRepository;
+        }
+
+        /// <summary>
+        /// Handles the specified @event.
+        /// </summary>
+        /// <param name="event">The @event.</param>
+        /// <returns></returns>
+        public Task Handle(ScenarioCreatedIntegrationEvent @event)
+        {
+            Scenario scenario = new Scenario(@event.Name);
+            this._scenarioRepository.Add(scenario);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Services/ScenarioManagement/ScenarioManagement.API/IntegrationEvents/Events/ScenarioCreatedIntegrationEvent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BuildingBlocks.EventBus.Events;
+
+namespace ScenarioManagement.API.IntegrationEvents.Events
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="BuildingBlocks.EventBus.Events.IntegrationEvent" />
+    public class ScenarioCreatedIntegrationEvent : IntegrationEvent
+    {
+        /// <summary>
+        /// Gets the scenario identifier.
+        /// </summary>
+        /// <value>
+        /// The scenario identifier.
+        /// </value>
+        public Guid ScenarioId { get; set; }
+
+        /// <summary>
+        /// Gets the schema identifier.
+        /// </summary>
+        /// <value>
+        /// The schema identifier.
+        /// </value>
+        public Guid SchemaId { get; set; }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+    }
+}
