291fd947b0e1ecc847de35fb7496514a922404ca
Justin Mackenzie <jmackenzie1908@gmail.com>
2017-07-17 22:24:01 -0400

    Implemented publish of event when a schema event is created. (#13)

--- src/Services/SchemaManagement/SchemaManagement.API/Application/Commands/CreateSchemaEventCommandHandler.cs
+++ src/Services/SchemaManagement/SchemaManagement.API/Application/Commands/CreateSchemaEventCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading.Tasks;
+using BuildingBlocks.EventBus.Abstractions;
 using MediatR;
+using SchemaManagement.API.IntegrationEvents.Events;
 using SchemaManagement.Domain;
 using Task = System.Threading.Tasks.Task;
 
@@ -9,16 +11,21 @@ namespace SchemaManagement.API.Application.Commands
     {
         private readonly ISchemaRepository _repository;
 
-        public CreateSchemaEventCommandHandler(ISchemaRepository repository)
+        private readonly IEventBus _eventBus;
+
+        public CreateSchemaEventCommandHandler(ISchemaRepository repository, IEventBus eventBus)
         {
             _repository = repository;
+            _eventBus = eventBus;
         }
 
         public Task<Event> Handle(CreateSchemaEventCommand message)
         {
-            Domain.Schema schema = this._repository.Get(message.SchemaId);
+            Schema schema = this._repository.Get(message.SchemaId);
             Event @event = schema.AddEvent(message.Name);
             this._repository.Update(schema);
+
+            this._eventBus.Publish(new EventCreatedIntegrationEvent(message.SchemaId, message.Name));
 
             return Task.FromResult(@event);
         }
---
+++ src/Services/SchemaManagement/SchemaManagement.API/IntegrationEvents/Events/EventCreatedIntegrationEvent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using BuildingBlocks.EventBus.Events;
+
+namespace SchemaManagement.API.IntegrationEvents.Events
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="BuildingBlocks.EventBus.Events.IntegrationEvent" />
+    public class EventCreatedIntegrationEvent : IntegrationEvent
+    {
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets the schema identifier.
+        /// </summary>
+        /// <value>
+        /// The schema identifier.
+        /// </value>
+        public Guid SchemaId { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventCreatedIntegrationEvent"/> class.
+        /// </summary>
+        /// <param name="schemaId">The schema identifier.</param>
+        /// <param name="name">The name.</param>
+        public EventCreatedIntegrationEvent(Guid schemaId, string name)
+        {
+            this.SchemaId = schemaId;
+            this.Name = name;
+        }
+    }
+}
