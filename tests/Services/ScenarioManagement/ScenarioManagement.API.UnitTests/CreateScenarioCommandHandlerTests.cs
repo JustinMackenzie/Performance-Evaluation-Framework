957f458fcec85df7b7e9ba0a4e674f2db9d2487c
Justin Mackenzie <jmackenzie1908@gmail.com>
2017-08-08 20:54:10 -0400

    Implemented first unit test for create scenario command handler. (#31)

---
+++ tests/Services/ScenarioManagement/ScenarioManagement.API.UnitTests/CreateScenarioCommandHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BuildingBlocks.EventBus.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ScenarioManagement.API.Application.Commands;
+using ScenarioManagement.Domain;
+
+namespace ScenarioManagement.API.UnitTests
+{
+    [TestClass]
+    public class CreateScenarioCommandHandlerTests
+    {
+        private Mock<IScenarioRepository> _repositoryMock;
+
+        private Mock<IEventBus> _eventBusMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this._repositoryMock = new Mock<IScenarioRepository>();
+            this._eventBusMock = new Mock<IEventBus>();
+        }
+
+        [TestMethod]
+        public void Handle_ValidCommand_CreatesScenarioWithCorrectName()
+        {
+            CreateScenarioCommand command = new CreateScenarioCommand { Name = "Test", SchemaId = Guid.NewGuid() };
+            this._repositoryMock
+                .Setup(r => r.Add(It.IsAny<Scenario>()))
+                .Returns(Task.CompletedTask)
+                .Callback<Scenario>(s =>
+                {
+                    Assert.AreEqual(command.Name, s.Name);
+                });
+
+            CreateScenarioCommandHandler handler = this.CreateHandler();
+
+            handler.Handle(command).Wait();
+        }
+
+        private CreateScenarioCommandHandler CreateHandler()
+        {
+            return new CreateScenarioCommandHandler(this._repositoryMock.Object, this._eventBusMock.Object);
+        }
+    }
+}
